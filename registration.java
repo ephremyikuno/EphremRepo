aae86c4fe3856ab4bb00587c209d3ee009276698
Ephrem Yikuno <ephremyikuno@gmail.com>
2018-05-11 21:43:42 -0400

    Changes

--- registration.java
+++ registration.java
@@ -4,6 +4,7 @@ public class registration {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     Webdriver driver = new ChromeDriver ();
+    //This is a GitHub example
 	}
 
 }
