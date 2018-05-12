42ed3af7acc66fa3ffae4b104702adf5b05300c9
Ephrem Yikuno <ephremyikuno@gmail.com>
2018-05-11 21:27:12 -0400

    New Class Added

---
+++ registration.java
@@ -0,0 +1,9 @@
+
+public class registration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Webdriver driver = new ChromeDriver ();
+	}
+
+}
---
+++ tess/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author EPHRE
+ *
+ */
+package tess;
